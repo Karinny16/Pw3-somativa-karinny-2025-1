7d0cb332b38fa5dcaa09a6eed09d56da651bc574
Karinny16 <167472526+Karinny16@users.noreply.github.com>
2025-02-20 22:38:31 -0300

    terceira aula
    NavBar e layout

--- Pw3-somativa-karinny-2025-1/pw3-somativa-2025-1/src/App.jsx
+++ Pw3-somativa-karinny-2025-1/pw3-somativa-2025-1/src/App.jsx
@@ -1,8 +1,7 @@
 
 import './App.css'
+import NavBar  from './components/layout/NavBar'
 
-import BookCard from './components/BookCard'
-import capa_livro from './assets/O iluminado.png'
 
 function App() {
  
@@ -10,28 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>PW3 - WEBAPP - LIVRARIA</h1>
-        <BookCard 
-        titulo='O iluminado'
-        autor='Stephen King'
-        imagem= {capa_livro}/>
-
-        {/* <BookCard 
-        titulo='1984'
-        autor='George Orwell'
-        imagem= 'uma imagem vai aparecer aqui'/>
-
-        <BookCard 
-        titulo='Os Miseráveis'
-        autor='Victor Hugo'
-        imagem= 'uma imagem vai aparecer aqui'/>
-
-        <BookCard 
-        titulo='A revolução dos bichos'
-        autor='George Orwell'
-        imagem= 'uma imagem vai aparecer aqui'/>
-        
-        */}
+        <NavBar> </NavBar>
 
       </div>
     </>

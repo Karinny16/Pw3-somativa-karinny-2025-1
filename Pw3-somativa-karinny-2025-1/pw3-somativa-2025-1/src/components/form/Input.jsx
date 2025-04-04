219a4bf3c8a6a5604d1f44f0a8b8444e0913f813
Karinny16 <167472526+Karinny16@users.noreply.github.com>
2025-04-03 21:51:32 -0300

    Add files via upload

--- Pw3-somativa-karinny-2025-1/pw3-somativa-2025-1/src/components/form/Input.jsx
+++ Pw3-somativa-karinny-2025-1/pw3-somativa-2025-1/src/components/form/Input.jsx
@@ -1,6 +1,6 @@
 import styles from './Input.module.css';
 
-function Input({type ,text, name, id, placeholder, value}){
+function Input({type ,text, name, id, placeholder, value, handlerChange}){
      return(
     <div className= {styles.form_control }>
 
@@ -11,6 +11,7 @@ function Input({type ,text, name, id, placeholder, value}){
         id={id}
         placeholder={placeholder}
         value={value}
+        onChange={handlerChange}
         />
         </div>
         )
